6027c83f7dde46cf0251b2e715dbd634ac2fb29e
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-04-06 21:42:39 +0200

    Adding requirement for account owner and tests to check that owner is present on init

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,14 @@
+require 'date'
 class Account
     STANDARD_VALIDITY_YRS = 5
-    attr_accessor :exp_date, :pin_code, :account_status, :balance
-    def initialize
+    attr_accessor :exp_date, :pin_code, :account_status, :balance, :owner
+    def initialize(attrs = {})
         @exp_date = set_expire_date
         @pin_code = generate_pin
         @account_status = :active
         @balance
+       # @owner = attrs[:owner] = nil ? missing_owner : attrs[:owner]
+        set_owner(attrs[:owner])
     end
 
     def set_expire_date
@@ -18,5 +21,14 @@ class Account
     
     def deactivate
         @account_status = :deactivate
+    end
+
+    private
+    def set_owner(obj)
+        obj == nil ? missing_owner : @owner = obj
+    end
+
+    def missing_owner
+        raise "An Account owner is required"
     end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,16 @@
 require './lib/account.rb'
-require './lib/atm.rb'
 describe Account do
-    
+    let(:person) {instance_double('Person', name: 'Steffe')}
+    subject {described_class.new({owner: person})}
+
+    it 'is expected to have an owner and ' do
+        expect(subject.owner).to eq person
+    end
+
+    it 'is expected to raise an error if no owner is set and ' do
+        expect {described_class.new }.to raise_error 'An Account owner is required'
+    end
+
     it 'is expected to have :active status on initialize' do
         expect(subject.account_status).to eq :active
     end
